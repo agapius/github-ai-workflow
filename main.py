e522091abc9b2d5dc9f6c5d3e8517c5bb3eb43d9
Jasper Castell <jasper.castell@octoenergy.com>
2025-03-28 09:16:09 +0100

    Add function to print "Good evening"
    This is a test to trigger the OpenAI review workflow.

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ def print_hi(name):
 def print_good_morning(name):
     print(f'Good morning, {name}')
 
+def print_good_evening(name):
+    print(f'Good evening, {name}')
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_good_morning('PyCharm')
+    print_good_evening('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
