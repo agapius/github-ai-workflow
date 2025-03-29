f5ccd06e631204cbc0b5f31a0629ccadab0328e1
Jasper Castell <jasper.castell@octoenergy.com>
2025-03-29 12:57:02 +0100

    Remove pycharm comments

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 # This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 def print_good_morning(name):
@@ -19,5 +14,3 @@ if __name__ == '__main__':
     print_hi('Jasper')
     print_good_morning('Jasper')
     print_good_evening('Jasper')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
