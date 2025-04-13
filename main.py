b9c9c9942307167f7fbc543797f4694acca06296
Jasper Castell <jasper.castell@kraken.tech>
2025-04-13 17:52:56 +0200

    Revert "Add stricter rules to prompt"
    This reverts commit f2559c81fb79471a8dbd3623b8d758e97522121d.

--- main.py
+++ main.py
@@ -12,13 +12,9 @@ def print_good_evening(name):
 def print_motivational_quote():
     print("All our dreams can come true, if we have the courage to pursue them")
 
-def print_another_motivational_quote():
-    print("Motivation will almost always beat mere talent")
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Jasper')
     print_good_morning('Jasper')
     print_good_evening('Jasper')
     print_motivational_quote()
-    print_another_motivational_quote()
